4bc297ebd226fb2dafdbf715daabbcc002c83b02
王宁 <wangning@wangningdeMacBook-Air.local>
2022-04-14 23:09:41 +0800

    增加了835类型代码

--- match_sink.py
+++ match_sink.py
@@ -9,14 +9,15 @@ from sink_CWE22 import sink_22
 from sink_CWE415 import sink_415, sink_416
 from sink_CWE617 import sink_617
 from sink_CWE772 import sink_772
+from sink_CWE835 import sink_835
 
-cwe = '787'  # 匹配的漏洞类型
-# old_file = '/Users/wangning/Documents/研一/跨函数测试/sink-source点匹配测试/已分析过漏洞/CWE-772/CWE-772/CVE-2017-11310/CVE-2017-11310_CWE-772_8ca35831e91c3db8c6d281d09b605001003bec08_png.c_1.1_OLD.c'
-# slice_file = '/Users/wangning/Documents/研一/跨函数测试/sink-source点匹配测试/已分析过漏洞/CWE-772/CWE-772/CVE-2017-11310/slices.txt'
-# diff_file = '/Users/wangning/Documents/研一/跨函数测试/sink-source点匹配测试/已分析过漏洞/CWE-772/CWE-772/CVE-2017-11310/CVE-2017-11310_CWE-772_8ca35831e91c3db8c6d281d09b605001003bec08_png.c_1.1.diff'
-old_file = "E:/漏洞检测/可自动化实现/自动化测试/imagemagick/CVE-2017-5510/CVE-2017-5510_CWE-787_91cc3f36f2ccbd485a0456bab9aebe63b635da88_psd.c_2.1_OLD.c"
-slice_file = "E:/漏洞检测/可自动化实现/自动化测试/imagemagick/CVE-2017-5510/slices.txt"
-diff_file = ''  # 只在匹配CWE-772类型时使用
+cwe = '835'  # 匹配的漏洞类型
+old_file = '/Users/wangning/Documents/研一/跨函数测试/sink-source点匹配测试/CWE835/ImageMagick/CVE-2015-8900/CVE-2015-8900_CWE-20_97aa7d7cfd2027f6ba7ce42caf8b798541b9cdc6_hdr.c_1.1_OLD.c'
+slice_file = '/Users/wangning/Documents/研一/跨函数测试/sink-source点匹配测试/CWE835/ImageMagick/CVE-2015-8900/slices.txt'
+diff_file = '/Users/wangning/Documents/研一/跨函数测试/sink-source点匹配测试/CWE835/ImageMagick/CVE-2015-8900/CVE-2015-8900_CWE-20_97aa7d7cfd2027f6ba7ce42caf8b798541b9cdc6_hdr.c_1.1.diff'
+# old_file = "E:/漏洞检测/可自动化实现/自动化测试/imagemagick/CVE-2017-5510/CVE-2017-5510_CWE-787_91cc3f36f2ccbd485a0456bab9aebe63b635da88_psd.c_2.1_OLD.c"
+# slice_file = "E:/漏洞检测/可自动化实现/自动化测试/imagemagick/CVE-2017-5510/slices.txt"
+# diff_file = ''  # 只在匹配CWE-772类型时使用
 list_key_words = ['if', 'while', 'for']  # 控制结构关键字
 # 变量类型列表
 val_type = ['short', 'int', 'long', 'char', 'float', 'double', 'struct', 'union', 'enum', 'const', 'unsigned', 'signed',
@@ -417,6 +418,19 @@ def match_sinks(slices):
 
     if cwe == '772' or cwe == '401':
         sink_772(old_file, sink_results, diff_file, loc)
+        for tmp_cv in cv_list[0]:
+            sink_cv_tmp = special_cv_process(tmp_cv)
+            if (len(sink_cv_tmp) > 1):
+                for i in range(1, len(sink_cv_tmp)):
+                    if sink_cv_tmp[i] not in sink_cv:
+                        sink_cv.append(sink_cv_tmp[i])
+
+            sink_cv.append(sink_cv_tmp[0])
+        print(sink_cv)
+        return sink_results, sink_cv, cv_list
+    
+    if cwe == '835':
+        sink_835(old_file, slices[1], sink_results, diff_file, loc)
         for tmp_cv in cv_list[0]:
             sink_cv_tmp = special_cv_process(tmp_cv)
             if (len(sink_cv_tmp) > 1):
--- sink_CWE835.py
+++ sink_CWE835.py
@@ -1,1 +1,95 @@
+from hashlib import new
+
+from urllib3 import Retry
 from share_func import *
+from sink_CWE772 import get_diff_message
+
+
+def is_con(line):
+    if('while ' in line):
+        return True
+    elif('for ' in line):
+        return True
+    elif('do ' in line):
+        return True
+    
+    return False
+
+def get_sink_line(vul_content, func_define, start_line):
+    can_res = False
+    func_define = func_define.split('location:')[0].replace(' ', '').strip()
+    print('func_define: ', func_define)
+    location = 0
+    flag = False #标记有没有到达漏洞函数
+    cnt = 0 #统计花括号的数量
+    will_be_cal = []
+
+    for line in vul_content:
+        location += 1
+        tmp_line = line.replace(' ', '').strip()
+        if(tmp_line == func_define):
+            flag = True
+        
+        if(flag == False):
+            continue
+        print(line, location)
+
+        if(location > start_line):
+            break
+
+        will_be_cal.append([line.strip(), location])
+    will_be_cal.reverse()
+    # print('will_be_cal: ', will_be_cal)
+
+    sign = False
+    for line in will_be_cal:
+        tmp_line = line[0].replace(' ', '').strip()
+        loc = line[1]    
+        if(tmp_line != '' and tmp_line[-1] == '}'):
+            cnt += 1
+            sign = True
+        if(tmp_line != '' and tmp_line[-1] == '{'):
+            cnt -= 1
+            sign = True
+
+        if((sign== True) and (cnt <= 0) and (is_con(line[0]))):
+            # print(line, loc)
+            can_res = True
+            return line[0].strip(), loc
+
+
+def sink_835(old_file, func_define, sink_results, diff_file, loc):
+    diff_mes = {}
+    with open(old_file, 'r') as f:
+        vul_content = f.readlines()
+    
+    with open(diff_file, 'r') as f:
+        diff_content = f.readlines()
+
+    num_fin = 0
+    diff_mes = get_diff_message(diff_content)
+
+    for start_line in diff_mes.keys():
+        num_list = diff_mes[start_line]
+        medium_tmp = num_list[0]
+        add_tmp = num_list[1]
+                                    
+        if(int(loc) > (int(start_line) + medium_tmp + add_tmp + 1)):
+            num_fin = add_tmp
+        elif(int(loc) >= (int(start_line) + medium_tmp)):#说明是在加号块中间的一句
+            already_num = int(loc) - (int(start_line) + medium_tmp + 1)
+            print(already_num, loc, start_line, medium_tmp)
+            num_fin += already_num
+            break
+                
+    print(loc, num_fin)
+    start_line = int(loc) - num_fin
+
+    print('将会从 ' + str(start_line) + '向上找sink点') # 寻找循环头所在的地方，这一行可能就是循环头
+
+    res_line, loc = get_sink_line(vul_content, func_define, start_line)
+    print(type(res_line))
+    print(type(loc))
+    new_line = res_line + 'location: ' + str(loc)
+    print(new_line)
+    sink_results.append(new_line)
